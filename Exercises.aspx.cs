4cce23634f555d29fb706086724b019cb42c1647
EvanOlbe <ejolbrych96@gmail.com>
2018-04-21 15:30:29 -0400

    Creation of Exercise page

---
+++ Exercises.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Exercises : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ddlEDropDown_DataBound(object sender, EventArgs e)
+    {
+        //ddlEDropDown.SelectedValue = "Select and exercise";
+    }
+    protected void ddlEDropDown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+}
